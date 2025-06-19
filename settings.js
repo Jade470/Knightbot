0d58afb8b857bd5a26ccd4af969c1d8ddf93ed75
Jade470 <polejade6@gmail.com>
2025-06-19 03:34:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Pole Jade Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'HIM', // Your name
   ownerNumber: '256746408974', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.3",
 };
